b77edef85a3ce1438886ae27541e4c6e2f24a8c2
Kyle <kyle.fields@gmail.com>
2023-03-08 08:29:27 -0500

    new commit

--- static/js/app.js
+++ static/js/app.js
@@ -28,7 +28,7 @@ data.forEach((dataRow) => {
 function handleClick() {
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-
+    console.log(date);
     // Check to see if a date was entered and filter the data using that date.
     if (date) {
         // Apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
@@ -40,7 +40,7 @@ function handleClick() {
 }
 
 // Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("Click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 //Build the table when the page loads.
 buildTable(tableData);
